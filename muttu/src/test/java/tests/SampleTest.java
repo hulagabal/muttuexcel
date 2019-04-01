5b0c72d31d1e7b777385eca80e8722556883d198
Mitturaj.h <Mitturaj.h@CPU1906>
2019-04-01 14:24:48 +0100

    Initial commit

---
+++ muttu/src/test/java/tests/SampleTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleTest() {
+
+		Assert.assertTrue(false);
+
+		System.out.println("Done+++++++++++++++++++++++");
+
+	}
+
+}
