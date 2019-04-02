66dd0b2b99be9d3916b39f0f868fc575a7a582a0
Mitturaj.h <Mitturaj.h@CPU1906>
2019-04-02 06:34:51 +0100

    added log file

--- muttu/src/test/java/tests/GetData.java
+++ muttu/src/test/java/tests/GetData.java
@@ -3,6 +3,8 @@ package tests;
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,12 +15,15 @@ import org.testng.annotations.Test;
 
 public class GetData {
 
+	private Logger logger = LogManager.getLogger(GetData.class);
+
 	public String[][] getData() {
 		String[][] strArray = null;
 		XSSFWorkbook book = null;
 
 		try {
-			book = new XSSFWorkbook(new FileInputStream(new File("C:\\Users\\Mitturaj.h\\Desktop\\data.xlsx")));
+			book = new XSSFWorkbook(
+					new FileInputStream(new File("C:\\Users\\Mitturaj.h\\muttu\\muttu\\Excel Files\\data.xlsx")));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -37,17 +42,11 @@ public class GetData {
 
 			int lastCellNumber = row.getLastCellNum();
 
-			System.out.println("Last Cell Number " + lastCellNumber);
-
 			for (int j = 0; j < lastCellNumber; j++) {
 
 				Cell ceelValue = row.getCell(j);
 
 				final DataFormatter df = new DataFormatter();
-
-				String valueAsString = df.formatCellValue(ceelValue);
-
-				System.out.println(valueAsString);
 
 				strArray[i][j] = df.formatCellValue(ceelValue);
 
@@ -68,6 +67,8 @@ public class GetData {
 
 	@Test(dataProvider = "data")
 	public void test1(String fname, String lname) {
+
+		logger.info(fname + "  " + lname);
 
 	}
 
